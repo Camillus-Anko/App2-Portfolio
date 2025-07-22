712130b7493111451c6605947b18b3c14b60d631
Camillus Anko <ankocamillus@gmail.com>
2025-07-22 15:33:24 +0000

    Create columns, image, my own image, and my own description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide") #Configururations about your page
+
+col1, col2 = st.columns(2) #Create two column objects
+
+with col1: #Open with a context manager
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Camillus Anko")
+    content = """
+    Hi, I am Camillus! I am a Python programmer, teacher, and founder of PythonHow. I graduated in 2013 with a Master of Science in Geospatial Technologies from the University of Muenster in Germany with a focus on using Python for remote sensing.
+I have worked with companies from various countries, such as the Center for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss in-Terra, and performing data mining to gain business insights with the Australian Rapid Intelligence.
+    """
+    st.info(content)
