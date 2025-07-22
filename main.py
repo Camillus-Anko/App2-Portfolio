e123dc3fad07881b6add850082fd8918f65fe8a3
Camillus Anko <ankocamillus@gmail.com>
2025-07-22 15:50:34 +0000

    Day 21:  Add description on the new columns

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ with col2:
 I have worked with companies from various countries, such as the Center for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss in-Terra, and performing data mining to gain business insights with the Australian Rapid Intelligence.
     """
     st.info(content)
+
+
+content2 = """
+Below you can find some of the apps I have built in Python. 
+Feel Free to contact me!
+"""
+
+st.info(content2)
+# st.write(content2)
